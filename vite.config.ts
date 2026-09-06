9df805db608cbfaeb8f526ec6933853178a64de2
Gulshan Toppo <gulshan1234g5@gmail.com>
2026-09-06 07:24:38 +0530

    build: target es2020, remove crossorigin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,33 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html
+          .replace(/<script type="module" crossorigin /g, '<script type="module" ')
+          .replace(/<link rel="stylesheet" crossorigin /g, '<link rel="stylesheet" ')
+      },
+    },
+  ],
   base: '/portfolio-fresh/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    target: 'es2020',
+    modulePreload: {
+      polyfill: true,
+    },
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 })
